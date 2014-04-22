e292942228e7d852060bf57de3959bd0df03e0f2
yut2@unlv.nevada.edu <yut2@unlv.nevada.edu@96b6cdd2-af3f-da45-1c9f-c668deddd353>
2014-04-22 01:04:43 +0000

    ...

--- trunk/DA6/DA6/DA6_Stepper Motor.c
+++ trunk/DA6/DA6/DA6_Stepper Motor.c
@@ -5,38 +5,44 @@
  *  Author: Ting Yu
  */ 
 
-#define F_CPU 8000000UL								//XTAL = 8 MHZ
-#include <avr/io.h>									//standard AVR header
-#include <util/delay.h>								//delay header
+#define F_CPU 8000000UL                             //XTAL = 8 MHZ
+#include <avr/io.h>                                   //standard AVR header
+#include <util/delay.h>                               //delay header
 void delay_ms(int count);
+
 
 int main()
 {
 	
-		DDRB	=	0xFF;						// B ports set
-		DDRD	=	0xFF;						// D ports set
-		DDRC	=	0x00;						// C ports set	
-		ADCSRA	=	0x87;			
-		ADMUX	=	0xC0;
+	DDRB    =   0xFF;                       // B ports set
+	DDRD    =   0xFF;                       // D ports set
+	DDRC    =   0x00;                       // C ports set
+	ADCSRA  =   0x87;
+	ADMUX   =   0xC0;
 	
 	while(1)
 	{
 		
 		ADCSRA |= (1<<ADSC);
-		while ((ADCSRA& (1<<ADIF)) == 0);		//loop begins		
+		while ((ADCSRA& (1<<ADIF)) == 0);     //loop begins
 		
-		int speed	=	ADC;					// declared int = ADC
-		speed+= 10;								
+		int speed   =   ADC;                    // declared int = ADC
+		speed+= 10;
 		
-		PORTB	= 0x66;							//port B set for speed of motor
-		_delay_ms(speed - 100);					//delay speed to 100ms
-		PORTB	= 0xCC;							//port B set for speed of motor
-		_delay_ms(speed - 100);					//delay speed to 100ms
-		PORTB	= 0x99;							//port B set for speed of motor
-		_delay_ms(speed - 100);					//delay speed to 100ms
-		PORTB	= 0x33;							//Port B set for speed of motor
-		_delay_ms(speed  -100);					//delay speed to 100ms
-	
+		PORTB   = 0x11;                         //port B set for speed of motor
+		delay_ms(speed);                  //delay speed 
+		PORTB   = 0x22;                         //port B set for speed of motor
+		delay_ms(speed);                  //delay speed 
+		PORTB   = 0x44;                         //port B set for speed of motor
+		delay_ms(speed);                  //delay speed 
+		PORTB   = 0x88;                         //Port B set for speed of motor
+		delay_ms(speed);                  //delay speed 
+		
 		
 	}
+}
+void delay_ms(int count)
+{
+	for (int i=0; i < count; i++)
+	_delay_ms(1);
 }
